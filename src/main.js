1669e37592406e1313a8ff9edf515b63551b6003
Yichuan Shen <shen.yichuan@gmail.com>
2018-03-03 17:39:29 +0100

    Add nearestNeighborMap()

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const nearestNeighborMap = require('./nearestNeighborMap')
+
+exports.map = function(data) {
+
+}
+
+Object.assign(module.exports, {nearestNeighborMap})
---
+++ src/nearestNeighborMap.js
@@ -0,0 +1,50 @@
+module.exports = function(data, sign) {
+    let height = data.length
+    let width = data.length > 0 ? data[0].length : 0
+    let map = [...Array(height)].map(_ => Array(width).fill(Infinity))
+    let min = Infinity
+
+    let f = (x, y) => {
+        if (data[y][x] === sign) min = 0
+        else if (data[y][x] === 0) min++
+        else min = Infinity
+
+        map[y][x] = min = Math.min(min, map[y][x])
+    }
+
+    for (let y = 0; y < height; y++) {
+        min = Infinity
+
+        for (let x = 0; x < width; x++) {
+            let old = Infinity
+
+            f(x, y)
+            old = min
+
+            for (let ny = y + 1; ny < height; ny++) f(x, ny)
+            min = old
+
+            for (let ny = y - 1; ny >= 0; ny--) f(x, ny)
+            min = old
+        }
+    }
+
+    for (let y = height - 1; y >= 0; y--) {
+        min = Infinity
+
+        for (let x = width - 1; x >= 0; x--) {
+            let old = Infinity
+
+            f(x, y)
+            old = min
+
+            for (let ny = y + 1; ny < height; ny++) f(x, ny)
+            min = old
+
+            for (let ny = y - 1; ny >= 0; ny--) f(x, ny)
+            min = old
+        }
+    }
+
+    return map
+}
