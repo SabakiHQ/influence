5dfbd54897b472162e8a89de14b6d70e5bf51cdb
Yichuan Shen <shen.yichuan@gmail.com>
2018-05-15 21:30:29 +0200

    Add test for holes or single point areas

--- tests/influenceMap.test.js
+++ tests/influenceMap.test.js
@@ -45,3 +45,26 @@ t.test('a stone at 3 3 should control the corner', t => {
 
     t.end()
 })
+
+t.test('should not have holes or single point areas', t => {
+    let result = influence.map(data.middle, {discrete: true})
+
+    for (let y = 0; y < data.middle.length; y++) {
+        for (let x = 0; x < data.middle[0].length; x++) {
+            if (data.middle[y][x] !== 0) continue
+
+            let neighbors = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]
+                .filter(([i, j]) => result[j] && result[j][i] != null)
+
+            if (neighbors.length === 0) continue
+
+            let sign = result[y][x] === 0 ? data.middle[neighbors[0][1]][neighbors[0][0]] : 0
+
+            if (result[y][x] === 0 && sign === 0) continue
+
+            t.notDeepEqual(neighbors.map(([i, j]) => result[j][i]), neighbors.map(_ => sign))
+        }
+    }
+
+    t.end()
+})
